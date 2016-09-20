7f6e385a708c791fa5066d8e86289491ae17dadc
Stephen Knutter <stephen.knutter@gmail.com>
2016-09-20 16:06:37 -0600

    adding production env

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,15 @@
 // Update with your config settings.
+require('dotenv').config();
+
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/personal_blog'
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL
   }
 
 };
