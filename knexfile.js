d0d3cabc120562a572ee105ef632c3a099e920c6
Stephen Knutter <stephen.knutter@gmail.com>
2016-09-20 19:08:50 -0600

    update knex for production

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,14 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
   }
 
 };
